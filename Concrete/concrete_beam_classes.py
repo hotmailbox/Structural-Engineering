cc318d7485b2d3e0de0b3d0cb1d12a19d4db6382
hotmailbox <35853549+hotmailbox@users.noreply.github.com>
2021-02-23 00:23:22 -0800

    Update concrete_beam_classes.py
    Make the script compatible for both Python 2 and 3

--- Concrete/concrete_beam_classes.py
+++ Concrete/concrete_beam_classes.py
@@ -26,7 +26,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from __future__ import division
 import math
-
+from sys import version_info
 class reinforcement:
     def __init__(self,fy_ksi):
         self.bar = {0:[0,0], 3:[0.375,0.11], 4: [0.5,0.2], 5: [0.625,0.31], 6: [0.75,0.44], 7: [0.875,0.6], 8: [1,0.79], 9: [1.128,1], 10: [1.27,1.27], 11: [1.41,1.56], 14: [1.693,2.25], 18: [2.257,4]}
@@ -527,8 +527,9 @@ class t_beam:
             else:
                 a = c
             loop+=1
-            print tension_c
-            print pna
+            
+            print(tension_c)
+            print(pna)
         return pna
 
     def moment_capacity_inlbs(self,bars_as_array,bars_d_array,bars_cg,c_in,fy_psi,Es_psi):
